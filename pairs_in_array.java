8c077e1ac9aa16dd3ceb2ab59867390c9754eead
Achyauta <achyautanandc@gmail.com>
2025-08-22 21:20:21 +0530

    Total Pairs in the array

---
+++ pairs_in_array.java
@@ -0,0 +1,24 @@
+// Pairs in an Array
+
+
+public class pairs_in_array { 
+
+// function for pairs 
+public static void printPairs(int arr[]){
+    for(int i=0;i<arr.length;i++){
+        int curr = arr[i];
+
+        for(int j=i+1;j<arr.length;j++){
+            System.out.println("("+ curr+" "+ arr[j]+")");
+        }
+    }
+}
+
+
+
+    public static void main(String[] args) {
+        int arr[] = {2,4,6,8,10};
+
+        printPairs(arr);
+    }
+}
