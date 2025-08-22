e8ed733f905e72011feb2d0f77fa1e0d291d8c16
Achyauta <achyautanandc@gmail.com>
2025-08-22 21:20:49 +0530

    Reverse an array

---
+++ reverse_array.java
@@ -0,0 +1,33 @@
+// REVERSE AND ARRAY
+
+
+public class reverse_array {
+
+//  function to print reverse
+public static void reverse(int arr[]){
+    int first = 0;
+    int last = arr.length-1;
+
+    while(first<last){
+        // swap
+        int temp  = arr[last];
+        arr[last]= arr[first];
+        arr[first] = temp;
+        first++;
+        last--;
+    }
+}
+
+
+public static void main(String[] args) {
+    int arr[] = { 2,4,6,8,10};
+
+    reverse(arr);
+
+    // print reverse array
+    for(int i=0;i<arr.length;i++){
+        System.out.println(arr[i]+" ");
+    }
+    System.out.println();
+}
+}
